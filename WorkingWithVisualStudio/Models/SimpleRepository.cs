7d1d3c76bf6b7d537d010b33462abf817f852376
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2019-12-09 20:02:27 +0200

    Listing 6-11

--- WorkingWithVisualStudio/Models/SimpleRepository.cs
+++ WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -27,6 +27,8 @@ namespace WorkingWithVisualStudio.Models
             {
                 AddProduct(p);
             }
+
+            products.Add("Error", null);
         }
 
         public IEnumerable<Product> Products => products.Values;
